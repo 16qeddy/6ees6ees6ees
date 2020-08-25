ed7f4662870a68af98b0c3e7acaa390d651df63c
charles Eddy <16qeddy@gmail.com>
2020-08-25 14:01:04 -0700

    all bees in order, assignment complete

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee{
+  constructor(){
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage(something){
+    if(something){
+      this.treasureChest.push(something);
+    } else {
+      this.treasureChest.push('treasure');
+    }
+  }
 };
